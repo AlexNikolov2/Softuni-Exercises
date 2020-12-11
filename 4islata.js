6767ebb308a6ff55e9129da4a9ddf48b49953efc
Aleks Nikolov <72394391+AlexNikolov2@users.noreply.github.com>
2020-12-12 00:28:56 +0200

    Delete 4islata.js

--- 4islata.js
+++
@@ -1,7 +0,0 @@
-function functionalna(number){
-    let number = Number(number)
-    let isValid = number >=100 && number <= 200 || n == 0
-    if(!isValid){
-        console.log("Invalid")
-    }
-}
