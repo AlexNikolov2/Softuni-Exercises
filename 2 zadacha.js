a006b60716ddb39482b9de35a8984b62b467d0da
Aleks Nikolov <72394391+AlexNikolov2@users.noreply.github.com>
2020-12-12 00:27:29 +0200

    Delete 2 zadacha.js

--- 2 zadacha.js
+++
@@ -1,9 +0,0 @@
-function multiply(arg){
-    let number = Number(arg)
-
-    for(let i = 1; i <=10; i++){
-        let result = i * number
-        console.log(`${i} * ${number} = ${result}`)
-    }
-}
-multiply(5)
